fe51ee1a8da9787f476bb37f9cf160a93e18f42f
zzz <534700646@qq.com>
2024-09-03 17:21:19 +0800

    新增部分代码

---
+++ test/src/main/java/test1.java
@@ -0,0 +1,11 @@
+import com.sun.tools.example.debug.expr.ExpressionParser;
+import org.springframework.expression.spel.standard.SpelExpressionParser;
+
+public class test1 {
+    public static void main(String[] args) {
+//        ExpressionParser parser = new SpelExpressionParser();
+//        Expression exp = parser.parseExpression("new String('hello world').toUpperCase()");
+//        String message = exp.getValue(String.class);
+//        System.out.println(message);
+    }
+}
